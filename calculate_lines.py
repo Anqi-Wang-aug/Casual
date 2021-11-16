72b5ca0c7074b8b55bb620b2e2c1b509af5128a8
Anqi Wang <angelwang11a@outlook.com>
2021-11-15 20:27:10 -0500

    Add files via upload

--- calculate_lines.py
+++ calculate_lines.py
@@ -1,26 +1,31 @@
+# a python program to calcualte number of non-empty lines in programs
+# paths of programs must be given path in a seperate input file. This input file must be put as command line arguments
+
 import sys
 import os
 
 if len(sys.argv) < 2:
 	raise ValueError('Please put some flies!')
-line = 0
+line_num= 0
 try:
 	input=open(sys.argv[1])
 	inputs = input.readlines()
 except Exception as e:
         print(e)
 
+
 print("Your input files:");
 for x in inputs:
 	x=x.strip() 
 	if os.path.exists(x):
 		try:
-			f=open(x)
-			text = f.readlines()
-			size = len(text)
-			line = line+size
 			print(x)
+			f=open(x)
+			text = (line.rstrip() for line in f)
+			text=list(line for line in text if line)
+			size = len(text)
+			line_num = line_num+size
 		except Exception as e:
 			print(e)
 
-print("You wrote " + str(line) + " lines of codes.")
+print("You wrote " + str(line_num) + " lines of codes.")
